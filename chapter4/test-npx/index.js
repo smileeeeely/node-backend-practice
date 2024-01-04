cc75a56f598d2faf4e8c1fb6a970948ffb0908d0
smileeeeely <minjoole.ivy@gmail.com>
2024-01-04 23:25:28 +0900

    npm-install-test

---
+++ chapter4/test-npx/index.js
@@ -0,0 +1,14 @@
+function getRamdomInt(
+    min,
+    max /*주석도 포매팅해줍니다.*/
+)
+{
+    return Math.floor(
+        Math.random()
+        *(max-min)) + min;
+}
+
+console.log(
+    getRamdomInt
+    (10,20);
+)
